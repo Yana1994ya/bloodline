b9bf1b089788cfddb8bc21617acc3be11aae4e1a
Yana Lisyuk <yana11114@gmail.com>
2021-03-02 22:25:24 +0200

    Add static resources.

--- blood/views.py
+++ blood/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def donation(request):
+
+	return render(request, "donation.html")
--- bloodline/urls.py
+++ bloodline/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blood.views import donation
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+		path('admin/', admin.site.urls),
+		path('donation/', donation, name="donation_start")
+	] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
