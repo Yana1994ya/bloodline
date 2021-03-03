2e53a0f4921744bf7d6ded22918ffc05b8cae601
Yana Lisyuk <yana11114@gmail.com>
2021-03-03 18:19:43 +0200

    Added all the base models.

--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,13 @@
+from django.db.models import Sum
 from django.shortcuts import render
+
+from blood.models import OutstandingDonations
 
 
 def homepage(request):
-    return render(request, "homepage.html")
+    outstanding = OutstandingDonations.objects \
+        .values('blood_type') \
+        .annotate(outstanding=Sum('units')) \
+        .order_by('blood_type')
+
+    return render(request, "homepage.html", {"outstanding": outstanding})
